5763a8bb8e8594a4b4e0e4cc9c5591292885b45d
Your Name <vikiamalindo7@gmail.com>
2024-06-14 14:25:31 +0700

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Provider } from 'react-redux';
+import App from './App';
+
+import './styles/style.css';
+import store from './states';
+
+const root = createRoot(document.getElementById('root'));
+
+// TODO: wrap App with store provider
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>,
+);
